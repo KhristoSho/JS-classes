8bbbab6f4d097d019dd5cd0dd149722d19a194c2
KhristSho <ShonusKhristo@yandex.ru>
2026-02-21 15:01:05 +0700

    add character and tests

---
+++ src/__tests__/character.test.js
@@ -0,0 +1,52 @@
+import { Character } from "../js/Character";
+
+
+test("test make a character", () => {
+  const expected = { 
+    name: "Ivan", 
+    type: "Bowman", 
+    health: 100, 
+    level: 1, 
+    attack: 25, 
+    defence: 25
+  };
+  expect(new Character("Ivan", "Bowman")).toEqual(expected);
+});
+
+test("test invalid name", () => {
+  expect(() => {
+    new Character("I", "Bowman");
+  }).toThrow(
+    "Name's lentgh should be more then 2 and less then 10 symbols"
+  );
+});
+
+test("test invalid type", () => {
+  expect(() => {
+    new Character("Ivan", "Human");
+  }).toThrow(
+    "Type should be one of Bowman, Swordsman, Magician, Daemon, Undead, Zombie"
+  );
+});
+
+test("test for character's level up", () => {
+  const character = new Character("Ivan", 'Magician');
+  character.levelUp();
+  expect(character.level).toBe(2);
+})
+
+test("test for dead character's level up for", () => {
+  expect(() => {
+    const character = new Character("Ivan", 'Magician');
+    character.health = 0;
+    character.levelUp()
+  }).toThrow(
+    "Cannot up level for dead character"
+  );
+})
+
+test("test character's damage", () => {
+  const character = new Character("Ivan", 'Magician');
+  character.damage(50);
+  expect(character.health).toBe(70);
+})
---
+++ src/js/character.js
@@ -0,0 +1,53 @@
+export class Character {
+
+  #attackDefence = {
+    "Bowman": [25, 25],
+    "Swordsman": [40, 10],
+    "Magician": [10, 40],
+    "Undead": [25, 25],
+    "Zombie": [40, 10],
+    "Daemon": [10, 40],
+  };
+
+  constructor(
+    name,
+    type,
+  ) {
+    this.name = this._validateName(name);
+    this.type = this._validateType(type);
+    this.health = 100;
+    this.level = 1;
+    this.attack = this.#attackDefence[type][0];
+    this.defence = this.#attackDefence[type][1];
+  }
+
+  _validateName(name) {
+    if (name.length < 2 || name.length > 10) {
+      throw new Error("Name's lentgh should be more then 2 and less then 10 symbols");     
+    }
+    return name;
+  }
+
+  _validateType(type) {
+    if (!["Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"].includes(type)) {
+      throw new Error("Type should be one of Bowman, Swordsman, Magician, Daemon, Undead, Zombie");
+    }
+    return type;
+  }
+  
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error("Cannot up level for dead character");
+    }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    }
+  }
+}
